e4d30eca10351439c791920c45190fb0d296ac2b
Vico <vpontin93@gmail.com>
2020-05-26 23:26:20 -0300

    Melhorias
    - Trata a msg removendo as mentions ao bot pelo nick dele;
    - Agora usa umas probabilidades básicas de mencionar na resposta, apenas responder sem mencionar e até mandar msg por conta em 20% das vezes que alguem postar algo;

--- main.js
+++ main.js
@@ -19,6 +19,17 @@ const bot = new discord_api.Client();
 /* Objeto principal do Markov Chain */
 var chain_principal = markov(1);				// Objeto de ordem 1
 
+/* Funções Úteis */
+// Função de gerar número aleatório
+function gerar_aleatorio(min, max) {
+	return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+// Remove mentions e etc de uma string
+function tratar_mensagem(msg) {
+	return msg.replace("<@!" + bot.user.id + ">", bot.user.username); // Substitui o código de mention pelo nick do BOT	
+}
+
 /* Lista inicial de frases para o Makrov Chain aprender */
 fs.readFileSync(__dirname + '\\frases.txt').toString().split("\n").forEach(function(line, index, arr) {
 	if (index === arr.length - 1 && line === "") { return; }
@@ -36,17 +47,42 @@ bot.once('ready', () => {
 bot.on('message', message => {
 	// Caso seja pingado, responde
 	if (message.mentions.has(bot.user)) {
-		message.reply(chain_principal.respond(message.content).join(' ')); // Cria uma resposta aleatória e junta as palavaras com espaços
+		// Caso não tenha sido ele mesmo que se mencionou
+		if (message.author !== bot.user) {
+			// 90% de probabilidade de mencionar a pessoa que marcou, senão apenas responde sem mention
+			if (Math.random() >= 0.1) {
+				message.reply(chain_principal.respond(message.content).join(' ')); // Cria uma resposta aleatória e junta as palavras com espaços
+				console.log('[INFO] O BOT respondeu!');
+			}
+			else
+			{
+				message.channel.send(chain_principal.respond(message.content).join(' ')); // Cria uma resposta aleatória e junta as palavras com espaços
+				console.log('[INFO] O BOT falou!');
+			}
+
+		}		
+	}
+	// Se outra pessoa mandar msg tem a 20% de chances de mandar uma msg de volta
+	else if (message.author !== bot.user)
+	{
+		if (Math.random() >= 0.8) {
+			message.channel.send(chain_principal.respond(message.content).join(' ')); // Cria uma resposta aleatória e junta as palavras com espaços
+			console.log('[INFO] O BOT falou por vontade própria!');
+		}
+		else
+		{
+			console.log("[INFO] Não foi dessa vez que o bot falou no canal...");
+		}
 	}
 	
 	// Caso o modo-aprendizado estiver habilitado e o usuário-alvo envie uma mensagem, registra no arquivo frases.txt para futuras mensaegns (OU já consegue usar? não sei se createReadStream atualiza caso o arquivo seja modificado)
 	if (config.discord.learn_mode && (message.author.id == config.discord.target_user)) {
-		chain_principal.seed(message.content);
-		fs.appendFile('frases.txt', message.content + '\n', 'utf8', function (err) {
+		chain_principal.seed(tratar_mensagem(message.content));
+		fs.appendFile('frases.txt', tratar_mensagem(message.content) + '\n', 'utf8', function (err) {
 			if (err) throw err;
 			console.log('[INFO] Mensagem armazenada no banco de dados!');
 		});
-	};
+	}
 });
 
 /* Loga no Discord com o token presente na config */
